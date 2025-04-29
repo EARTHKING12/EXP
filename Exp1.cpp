7cfc18010fea00b4651e2d8c664ce2d234f9875f
Pruthviraj Rathod <pruthvirajrathod645@gmail.com>
2025-04-29 18:43:55 +0530

    Update Exp1.cpp

--- Exp1.cpp
+++ Exp1.cpp
@@ -1,109 +1,112 @@
 #include<iostream>
 using namespace std;
-#define MAX 100;
-class Hashing{
-    public:
-    long long arr[100],n,key;
-    string arr2[100];
+#define MAX 100
+
+class Hashing {
+public:
+    long long arr[MAX], n, key;
+    string arr2[MAX];
     string name;
-    void compalsary()
-    {
-        cout<<"Enter the size of table";
-        cin>>n;
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=-1;
-            arr2[i]=-1;
+
+    void compalsary() {
+        cout << "Enter the size of table: ";
+        cin >> n;
+        for(int i = 0; i < n; i++) {
+            arr[i] = -1;
+            arr2[i] = ""; // initialize as empty string
         }
     }
-    void display()
-    {
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]!=-1)
-            {
-                cout<<i<<"--->"<<arr2[i];
+
+    void display() {
+        cout << "\n--- Hash Table ---\n";
+        for(int i = 0; i < n; i++) {
+            if(arr[i] != -1) {
+                cout << i << " ---> " << arr2[i] << endl;
             }
         }
     }
-    void insert()
-    {
-        cout<<"Enter the key";
-        cin>>key;
-        cout<<"Enter the name";
-        cin>>name;
-        int index=key%n;
-        if(arr[index]!=-1)
-        {
-            for(int i=0;i<n;i++)
-            {
-                if(arr[i]==0)
-                {
-                    arr[i]=key;
-                    arr2[i]=name;
+
+    void insert() {
+        cout << "Enter the key (number): ";
+        cin >> key;
+        cout << "Enter the name: ";
+        cin >> name;
+        
+        int index = key % n;
+        if(arr[index] != -1) { // collision
+            bool inserted = false;
+            for(int i = 0; i < n; i++) {
+                if(arr[i] == -1) { // empty spot found
+                    arr[i] = key;
+                    arr2[i] = name;
+                    inserted = true;
+                    cout << "Inserted at index " << i << " after collision.\n";
+                    break;
                 }
             }
-            cout<<"Table is full";
+            if(!inserted) {
+                cout << "Table is full. Cannot insert.\n";
+            }
         }
-        else{
-            arr[index]=key;
-            arr2[index]=name;
+        else { // no collision
+            arr[index] = key;
+            arr2[index] = name;
+            cout << "Inserted at index " << index << ".\n";
         }
     }
-    void Delete()
-    {
-        int num,c=0;
-        cout<<"Enter  number to delete";
-        cin>>num;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==num)
-            {
-                arr[i]=-1;
-                arr2[i]=-1;
-                c=0;
+
+    void Delete() {
+        int num, c = 0;
+        cout << "Enter number to delete: ";
+        cin >> num;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == num) {
+                arr[i] = -1;
+                arr2[i] = "";
+                c = 0;
+                cout << "Deleted successfully.\n";
+                return;
+            }
+            else {
+                c = 1;
+            }
+        }
+        if(c == 1) {
+            cout << "Number not found in table.\n";
+        }
+    }
+
+    void search() {
+        int number;
+        cout << "Enter number to search: ";
+        cin >> number;
+        bool found = false;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == number) {
+                cout << "Number found at index " << i << ".\n";
+                found = true;
                 break;
             }
-            else
-            {
-                c=1;
-            }
         }
-        if(c==1)
-        {
-            cout<<"Number is not in list";
-        }
-    }
-    void search()
-    {
-        int number;
-        cout<<"Enter number to search";
-        cin>>number;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==number)
-            {
-                cout<<"Number is found at index"<<i;
-            }
+        if(!found) {
+            cout << "Number not found.\n";
         }
     }
 };
-int main()
-{
-    int choice;
+
+int main() {
+    int choice = 0;
     Hashing obj;
     obj.compalsary();
-    while(choice!=5)
-    {
-        cout<<"\n1.Insert";
-        cout<<"\n2.Delete";
-        cout<<"\n3.Search";
-        cout<<"\n4.display";
-        cout<<"\n5.Exit";
-        cout<<"\nEnter your choice::";
-        cin>>choice;
-        switch(choice)
-        {
+    while(choice != 5) {
+        cout << "\n1. Insert";
+        cout << "\n2. Delete";
+        cout << "\n3. Search";
+        cout << "\n4. Display";
+        cout << "\n5. Exit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        switch(choice) {
             case 1:
                 obj.insert();
                 break;
@@ -111,13 +114,17 @@ int main()
                 obj.Delete();
                 break;
             case 3:
-                NULL;
+                obj.search();
                 break;
             case 4:
                 obj.display();
                 break;
             case 5:
-                cout<<"Exit";
+                cout << "Exiting Program.\n";
+                break;
+            default:
+                cout << "Invalid choice. Try again.\n";
         }
     }
+    return 0;
 }
