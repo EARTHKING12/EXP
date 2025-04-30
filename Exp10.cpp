8aadede23e59dc589c98bd4791a92a1808c7e9f5
Pruthviraj Rathod <pruthvirajrathod645@gmail.com>
2025-04-30 08:44:28 +0530

    Update Exp10.cpp

--- Exp10.cpp
+++ Exp10.cpp
@@ -10,8 +10,6 @@ Implement the priority queue to cater services to the patients
 INPUT: Patient with the severity of desease 
 OUTPUT: A priority queue 
 
-Author: Dr. Sunil Damodar Rathod.
-Date: 26.05.2023
 */
 
 #include<iostream>
@@ -154,256 +152,117 @@ int main(void)
 }
 /*
 
-D:\ICEM\AY-2022-23\SE\DSAL_Lab>a
+#include<iostream>
+#include<cstring>
+#include<ios>
+#include<limits>
 
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:1
+using namespace std;
 
-Enter patient ID:1
+typedef struct patient {
+	int patientID;
+	int severity;
+} PATIENT;
 
-Enter severity
-        2. Top
-        1. Medium
-        0. Least
-        ::0
+class priorityQ {
+	private:
+		int nop;
+		PATIENT P[50];
+		const char priority[3][10] = { "Least", "Medium", "Top" };
+	public:
+	priorityQ() {
+		for (int i = 0; i < 50; i++) {
+			P[i].patientID = 0;
+			P[i].severity = 0;
+		}
+		nop = 0;
+	}
 
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
+	void displayQueue() {
+		for (int i = 1; i <= nop; i++)
+			cout << "\n" << P[i].patientID << "  " << priority[P[i].severity];
+	}
 
-1  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:1
+	void addPatient(PATIENT p) {
+		int i;
+		PATIENT t;
+		i = nop;
+		if (i == 0) {
+			P[++i] = p;
+			nop = i;
+		} else {
+			P[++i] = p;
+			nop = i;
+			while (i > 1 && P[i].severity > P[i / 2].severity) {
+				t = P[i];
+				P[i] = P[i / 2];
+				P[i / 2] = t;
+				i = i / 2;
+			}
+		}
+	}
 
-Enter patient ID:2
+	void deletePatient() {
+		PATIENT t;
+		int i, j;
+		t = P[1];
+		P[1] = P[nop];
+		P[nop] = t;
+		nop--;
+		i = 1;
+		while (2 * i <= nop) {
+			j = 2 * i;
+			if (j + 1 <= nop && P[j + 1].severity > P[j].severity)
+				j++;
+			if (P[i].severity < P[j].severity) {
+				t = P[i];
+				P[i] = P[j];
+				P[j] = t;
+				i = j;
+			} else
+				break;
+		}
+	}
+};
 
-Enter severity
-        2. Top
-        1. Medium
-        0. Least
-        ::1
+int main(void) {
+	priorityQ B;
+	int cho, n, cho1;
+	PATIENT p;
+	char key[30];
+	cho = 0;
+	while (cho != 4) {
+		cout << "\n************PATIENT QUEUE*************";
+		cout << "\n1-ADD PATIENT TO QUEUE";
+		cout << "\n2-DISPLAY QUEUE";
+		cout << "\n3-DELETE PATIENT FROM QUEUE";
+		cout << "\n4-EXIT";
+		cout << "\nEnter ur choice:";
+		cin >> cho;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		switch (cho) {
+			case 1:
+				cout << "\nEnter patient ID:";
+				cin >> p.patientID;
+				cout << "\nEnter severity\n\t2. Top \n\t1. Medium \n\t0. Least\n\t::";
+				cin >> n;
+				p.severity = n;
+				B.addPatient(p);
+				break;
+			case 2:
+				B.displayQueue();
+				break;
+			case 3:
+				B.deletePatient();
+				break;
+			case 4:
+				cout << "\nProgram Exits!!";
+				break;
+			default:
+				cout << "\nWrong choice!";
+		}
+	}
+	return 0;
+}
 
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-2  Medium
-1  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:1
-
-Enter patient ID:3
-
-Enter severity
-        2. Top
-        1. Medium
-        0. Least
-        ::0
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-2  Medium
-1  Least
-3  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:1
-
-Enter patient ID:4
-
-Enter severity
-        2. Top
-        1. Medium
-        0. Least
-        ::2
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-4  Top
-2  Medium
-3  Least
-1  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:3
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-2  Medium
-1  Least
-3  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:3
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-3  Least
-1  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:1
-
-Enter patient ID:5
-
-Enter severity
-        2. Top
-        1. Medium
-        0. Least
-        ::2
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-5  Top
-1  Least
-3  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:1
-
-Enter patient ID:6
-
-Enter severity
-        2. Top
-        1. Medium
-        0. Least
-        ::1
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-5  Top
-6  Medium
-3  Least
-1  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:3
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-6  Medium
-1  Least
-3  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:3
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-3  Least
-1  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:3
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-1  Least
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:3
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:2
-
-************PATIENT QUEUE*************
-1-ADD PATIENT TO QUEUE
-2-DISPLAY QUEUE
-3-DELETE PATIENT FROM QUEUE
-4-EXIT
-Enter ur choice:
 */
